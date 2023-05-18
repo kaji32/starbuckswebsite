a3a34cd7153a192e36d934747474ef958fd32012
kaji32 <73316284+kaji32@users.noreply.github.com>
2023-05-18 09:40:43 +0900

    Add files via upload

---
+++ starbucksLP/js/script.js
@@ -0,0 +1,8 @@
+function imgSlider(anything){
+    document.querySelector('.starbucks').src = anything
+}
+
+function changeCircle(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
